4ca13009635cf3af0a9ec1b79d17c1ccc67d46af
ppoile <andi@daemeier-clan.ch>
2014-10-13 23:38:54 +0200

    implement stadion index and details

--- main/views.py
+++ main/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from main.models import Stadion
 
 def index(request):
     return HttpResponse("Hello, world. You're at the main index.")
 
 def sindex(request):
-    stadions = Stadion.objects.all()
-    output = ", ".join([item.name for item in stadions])
-    return HttpResponse(output)
+    stadions_list = Stadion.objects.all()
+    template = loader.get_template("main/stadion_index.html")
+    context = RequestContext(request, dict(stadions_list=stadions_list))
+    return HttpResponse(template.render(context))
 
 def sdetail(request, stadion_id):
-    return HttpResponse("Hello, world. You're looking at detail for stadion %s." % stadion_id)
+    stadion = Stadion.objects.get(pk=stadion_id)
+    template = loader.get_template("main/stadion_details.html")
+    context = RequestContext(request, dict(stadion=stadion))
+    return HttpResponse(template.render(context))
