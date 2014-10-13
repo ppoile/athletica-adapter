f583cf7094f270818f5fae7ab67ecdb49d0a5f8b
ppoile <andi@daemeier-clan.ch>
2014-10-13 17:44:51 +0200

    db: adapt to athletica

--- athletica/settings.py
+++ athletica/settings.py
@@ -58,8 +58,11 @@ WSGI_APPLICATION = 'athletica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'athletica',
+        'USER': 'athletica',
+        'PASSWORD': 'athletica',
+        'HOST': 'localhost',
     }
 }
 
