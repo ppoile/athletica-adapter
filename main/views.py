22b1103b311ebfe5dc4513fb104bff8828ddbdac
ppoile <andi@daemeier-clan.ch>
2014-10-13 23:48:49 +0200

    raise Http404 upon non-existent stadion_id

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from main.models import Stadion
 
@@ -10,6 +11,9 @@ def sindex(request):
     return render(request, "main/stadion_index.html", context)
 
 def sdetail(request, stadion_id):
-    stadion = Stadion.objects.get(pk=stadion_id)
+    try:
+        stadion = Stadion.objects.get(pk=stadion_id)
+    except Stadion.DoesNotExist:
+        raise Http404
     context = dict(stadion=stadion)
     return render(request, "main/stadion_details.html", context)
