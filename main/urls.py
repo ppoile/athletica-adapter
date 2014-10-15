8b4d1aa50494038e734e02e1852f14c59b35d1c0
ppoile <andi@daemeier-clan.ch>
2014-10-15 22:40:35 +0200

    anmeldungen: add url, view and template

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^stadion/$', views.sindex, name='sindex'),
     url(r'^stadion/(?P<stadion_id>\d+)/$', views.sdetail, name='sdetail'),
-
+    url(r'^meeting/(?P<meeting_id>\d+)/anmeldungen/$', views.anmeldungen, name="anmeldungen"),
 )
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render
+from main.models import Meeting
 from main.models import Stadion
 
 def index(request):
@@ -14,3 +15,17 @@ def sdetail(request, stadion_id):
     anlagen = [item.bezeichnung for item in stadion.anlage_set.all()]
     context = dict(stadion=stadion, anlagen=anlagen)
     return render(request, "main/stadion_details.html", context)
+
+def anmeldungen(request, meeting_id):
+    meeting = get_object_or_404(Meeting, pk=meeting_id)
+    anmeldungen = [(anmeldung.startnummer,
+                    anmeldung.athlet.vorname,
+                    anmeldung.athlet.name,
+                    anmeldung.athlet.verein.name) for anmeldung in meeting.anmeldungen.all()]
+    context = dict(meeting=meeting, anmeldungen=anmeldungen)
+    return render(request, "main/anmeldungen.html", context)
+
+[(anmeldung.startnummer,
+  anmeldung.athlet.vorname,
+  anmeldung.athlet.name,
+  anmeldung.athlet.verein.name) for anmeldung in Meeting.objects.first().anmeldungen.filter(athlet__vorname="Cedric")]
