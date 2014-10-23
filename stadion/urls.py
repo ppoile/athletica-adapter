6d4beab1e7550c4c8e1e920ae595b567cb81debe
ppoile <andi@daemeier-clan.ch>
2014-10-23 21:34:14 +0200

    add voting form in stadion:details

--- stadion/urls.py
+++ stadion/urls.py
@@ -5,4 +5,5 @@ from stadion import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<stadion_id>\d+)/$', views.details, name='details'),
+    url(r'^(?P<stadion_id>\d+)/vote/$', views.vote, name='vote'),
 )
--- stadion/views.py
+++ stadion/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from main.models import Anlage
 from stadion.models import Stadion
 
 def index(request):
@@ -11,3 +14,18 @@ def details(request, stadion_id):
     context = dict(stadion=stadion)
     return render(request, "stadion/details.html", context)
 
+def vote(request, stadion_id):
+    stadion = get_object_or_404(Stadion, pk=stadion_id)
+    try:
+        selected_anlage = stadion.anlagen.get(pk=request.POST['anlage'])
+    except (KeyError, Anlage.DoesNotExist):
+        # Redisplay the question voting form.
+        context = dict(stadion=stadion, error_message="You did not select a choice.")
+        return render(request, 'stadion/details.html', context)
+    else:
+        #selected_anlage.votes += 1
+        #selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('stadion:details', args=(stadion.xstadion,)))
