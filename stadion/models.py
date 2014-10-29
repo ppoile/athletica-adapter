89788dff5f8edc6d4bb13adebe3cc8194819e342
ppoile <andi@daemeier-clan.ch>
2014-10-29 21:43:45 +0100

    stadion: models: add choices

--- stadion/models.py
+++ stadion/models.py
@@ -5,8 +5,8 @@ class Stadion(models.Model):
     name = models.CharField(db_column='Name', max_length=50)  # Field name made lowercase.
     bahnen = models.IntegerField(db_column='Bahnen')  # Field name made lowercase.
     bahnengerade = models.IntegerField(db_column='BahnenGerade')  # Field name made lowercase.
-    ueber1000m = models.CharField(db_column='Ueber1000m', max_length=1)  # Field name made lowercase.
-    halle = models.CharField(db_column='Halle', max_length=1)  # Field name made lowercase.
+    ueber1000m = models.CharField(db_column='Ueber1000m', max_length=1, choices=(('y', True), ('n', False)), default='n')  # Field name made lowercase.
+    halle = models.CharField(db_column='Halle', max_length=1, choices=(('y', True), ('n', False)), default='n')
 
     def __str__(self):
         return self.name
