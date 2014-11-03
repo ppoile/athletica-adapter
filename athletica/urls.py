6c34859c37e7f12bb928a1e5d9322b7a444ca75d
ppoile <andi@daemeier-clan.ch>
2014-11-03 20:48:28 +0100

    add root index page

--- athletica/urls.py
+++ athletica/urls.py
@@ -1,8 +1,24 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("""
+<nav>
+  <ul>
+    <li><a href="/admin">Administration</a></li>
+    <li><a href="/stadion">Stadien</a></li>
+    <li><a href="/meeting">Meetings</a></li>
+    <li><a href="/main">Main</a></li>
+  </ul>
+</nav>
+""")
+
 
 urlpatterns = patterns(
     '',
+    url(r'^$', index, name='root-index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^main/', include('main.urls', namespace="main")),
     url(r'^meeting/', include('meeting.urls', namespace="meeting")),
