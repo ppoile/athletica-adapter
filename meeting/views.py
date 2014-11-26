1f3a541fe192a6d96e534b7509d90d086e3eb038
Andreas Meier <andi@daemeier-clan.ch>
2014-11-26 06:05:30 +0100

    meeting: implement reportlab pdf rangliste

--- meeting/views.py
+++ meeting/views.py
@@ -7,7 +7,13 @@ from main.models import Start
 from main.models import Wettkampf
 from meeting.models import Meeting
 from meeting.rangliste import RanglistenItem, Rangliste
+from reportlab.lib import colors
+from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER, TA_JUSTIFY
+from reportlab.lib.pagesizes import A3, A4, landscape, portrait
+from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
+from reportlab.lib.units import cm
 from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Frame, Spacer
 
 
 class Index(generic.ListView):
@@ -71,18 +77,47 @@ def rangliste(request, meeting_id, wettkampf_info, kategorie_name):
     return render(request, "meeting/rangliste.html", context)
 
 def rangliste_pdf(request, meeting_id, wettkampf_info, kategorie_name):
-    # Create the HttpResponse object with the appropriate PDF headers.
+    meeting_wettkaempfe = Wettkampf.objects.filter(meeting_id=meeting_id)
+    wettkaempfe = meeting_wettkaempfe.filter(
+        info=wettkampf_info, kategorie__name=kategorie_name)
+    num_wettkaempfe = wettkaempfe.count()
+    num_wettkaempfe_info_mapping = {
+        4: "Vier",
+        5: u"Fünf",
+        6: "Sechs",
+        7: "Sieben",
+        10: "Zehn",
+    }
+    wettkampf_name = "%s, %skampf" % (
+        kategorie_name, num_wettkaempfe_info_mapping[num_wettkaempfe])
+
+    meeting_starts = Start.objects.filter(wettkampf__meeting_id=meeting_id)
+    starts = meeting_starts.filter(wettkampf__info=wettkampf_info,
+                                   wettkampf__kategorie__name=kategorie_name)
+    ordered_starts = starts.order_by("anmeldung__athlet")
+    rangliste = Rangliste()
+    for start in ordered_starts:
+        rangliste.add_start(start)
+
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+    response['Content-Disposition'] = 'attachment; filename="rangliste.pdf"'
 
-    # Create the PDF object, using the response object as its "file."
-    p = canvas.Canvas(response)
+    doc = SimpleDocTemplate(response, pagesize=A4)
 
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world.")
+    elements = []
+    styles=getSampleStyleSheet()
+    styleN = styles["Normal"]
 
-    # Close the PDF object cleanly, and we're done.
-    p.showPage()
-    p.save()
+    headings = ["Rang", "Name", "Jg", "Verein", "Land", "Punkte", "Bem"]
+    data = [headings]
+    for rang, start in rangliste.get():
+        data.append([rang, start.name, start.jahrgang, start.verein,
+                     start.land, start.punkte, start.bem])
+                     #start.disziplinen_list_text])
+
+    tableThatSplitsOverPages = Table(data, [2.5 * cm, 2.5 * cm], repeatRows=1)
+    elements.append(tableThatSplitsOverPages)
+
+    doc.build(elements)
+
     return response
