53283a50eb9173c91771373ec7466db4426e1023
Andreas Meier <andi@daemeier-clan.ch>
2014-11-27 01:54:33 +0100

    meeting: rework and factor out Disziplin classes

--- meeting/rangliste.py
+++ meeting/rangliste.py
@@ -1,11 +1,65 @@
+class Rangliste(object):
+    def __init__(self):
+        self._items = dict()
+
+    def add_starts(self, starts):
+        for start in starts:
+            self._add_start(start)
+
+    def _add_start(self, start):
+        serienstart = start.serienstart.first()
+        if serienstart is None:
+            return
+
+        disziplin = start.wettkampf.disziplin.kurzname
+        reihenfolge = start.wettkampf.mehrkampfreihenfolge
+        windmessung = start.wettkampf.windmessung
+        last_disziplin = start.wettkampf.mehrkampfende
+
+        resultate = serienstart.resultat.order_by("-punkte").all()
+        for resultat in resultate:
+            leistung = resultat.leistung
+            wind = ""
+            if windmessung:
+                wind = serienstart.serie.wind
+                if wind == "":
+                    wind = resultat.info
+            punkte = int(resultat.punkte)
+            if not resultat.info.lower().endswith("x"):
+                break
+
+        item = self._get_item(start.anmeldung.athlet)
+        item.add_disziplin(disziplin, reihenfolge, last_disziplin, leistung,
+                           wind, punkte)
+
+    def _get_item(self, athlet):
+        """get ranglisten item
+
+        Get existing or new ranglisten item.
+        """
+        try:
+            item = self._items[athlet.id]
+        except KeyError:
+            item = RanglistenItem(athlet.name, athlet.vorname, athlet.jahrgang,
+                                  athlet.verein, athlet.land, "")
+            self._items[athlet.id] = item
+        return item
+
+    def get(self):
+        items = sorted(self._items.values(), reverse=True)
+        rangliste = []
+        for item in items:
+            if item._num_valid_disziplinen() < len(item._disziplinen):
+                rang = ""
+            elif len(rangliste) > 0 and item == rangliste[-1][1]:
+                rang = rangliste[-1][0]
+            else:
+                rang = "%d." % (len(rangliste) + 1)
+            rangliste.append((rang, item))
+        return rangliste
+
+
 class RanglistenItem(object):
-    def _get_leistung_divisor(self, disziplin):
-        if disziplin[0].isdigit():
-            return 1000
-        return 100
-
-    _LEISTUNG_MAPPING = { -1: "n.a.", -2: "aufg.", -3: "dis." }
-
     def __init__(self, name, vorname, jahrgang, verein, land, bem):
         self._name = "%s %s" % (name, vorname)
         self._jahrgang = "%02d" % (jahrgang % 100)
@@ -42,55 +96,13 @@ class RanglistenItem(object):
     def add_disziplin(self, disziplin, reihenfolge, last_disziplin, leistung,
                       wind, punkte):
         assert self._disziplinen.get(disziplin) is None
-        self._disziplinen[(reihenfolge, disziplin)] = \
-            dict(leistung=leistung, wind=wind, punkte=punkte,
-                 last_disziplin=last_disziplin)
+        self._disziplinen[disziplin] = create_disziplin(
+            name=disziplin, reihenfolge=reihenfolge, leistung=leistung,
+            wind=wind, punkte=punkte, last_disziplin=last_disziplin)
         self._punkte += punkte
 
-    @property
     def disziplinen_list_text(self):
-        disziplinen_texts = []
-        last_disziplinen_text = None
-        for (reihenfolge, disziplin), resultat in sorted(
-                self._disziplinen.iteritems()):
-            last_disziplin = resultat.pop("last_disziplin")
-            disziplinen_text = self._get_disziplinen_text(disziplin,
-                                                          **resultat)
-            if last_disziplin:
-                last_disziplinen_text = disziplinen_text
-            else:
-                disziplinen_texts.append(disziplinen_text)
-        if last_disziplinen_text is not None:
-            disziplinen_texts.append(last_disziplinen_text)
-        text = ", ".join(disziplinen_texts)
-        return text
-
-    def _get_disziplinen_text(self, disziplin, leistung, wind, punkte):
-        text = "%s" % disziplin
-        if leistung in self._LEISTUNG_MAPPING:
-            text += " (%s, %s)" % (leistung, self._LEISTUNG_MAPPING[leistung])
-            assert punkte == 0
-            return text
-
-        divisor = self._get_leistung_divisor(disziplin)
-        if divisor == 1000:
-            hours, remainder = divmod(float(leistung) / divisor, 3600)
-            minutes, seconds = divmod(remainder, 60)
-            timestring = "%d:%d:%.2f" % (hours, minutes, seconds)
-            timestring = timestring.lstrip("0:")
-            #print "hours=%d, minutes=%d, seconds=%.3f => %s" % (
-            #    hours, minutes, seconds, timestring)
-            text += " (%s" % timestring
-        else:
-            text += " (%.2f" % (float(leistung) / divisor)
-        if wind not in ["", "----"] :
-            wind = wind.rstrip(" m*")
-            wind = wind.replace(",", ".")
-            wind = float(wind)
-            text += " / %s"  % wind
-        punkte = int(punkte)
-        text += ", %s)"  % punkte
-        return text
+        return ", ".join([str(d) for d in sorted(self._disziplinen.values())])
 
     def __cmp__(self, other):
         # compare number of completed disziplines
@@ -110,9 +122,9 @@ class RanglistenItem(object):
         # compare diszipline wins
         wins = 0
         other_wins = 0
-        for reihenfolge_disziplin, resultat in self._disziplinen.iteritems():
-            tmp = cmp(resultat["leistung"],
-                      other._disziplinen[reihenfolge_disziplin]["leistung"])
+        for disziplin, resultat in self._disziplinen.iteritems():
+            tmp = cmp(resultat.leistung,
+                      other._disziplinen[disziplin].leistung)
             if tmp > 0:
                 wins += 1
             if tmp < 0:
@@ -123,8 +135,8 @@ class RanglistenItem(object):
             return value
 
         # compare highest punkte
-        punkte = [item["punkte"] for item in self._disziplinen.values()]
-        other_punkte = [item["punkte"] for item in other._disziplinen.values()]
+        punkte = [d.punkte for d in self._disziplinen.values()]
+        other_punkte = [d.punkte for d in other._disziplinen.values()]
         value = cmp(punkte, other_punkte)
         #print "4: cmp(%s, %s) => %d" % (punkte, other_punkte, value)
         return value
@@ -132,66 +144,100 @@ class RanglistenItem(object):
     def _num_valid_disziplinen(self):
         num = 0
         for disziplin in self._disziplinen.values():
-            if disziplin["leistung"] not in [-1, -3]:
+            if disziplin.leistung not in [-1, -3]:
                 num += 1
         return num
 
 
-class Rangliste(object):
-    def __init__(self):
-        self._starts = dict()
+class DisziplinBase(object):
+    _LEISTUNG_MAPPING = { -1: "n.a.", -2: "aufg.", -3: "dis." }
 
-    def add_starts(self, starts):
-        for start in starts:
-            self._add_start(start)
+    def __init__(self, name, reihenfolge, leistung, wind, punkte,
+                 last_disziplin):
+        self._name = name
+        self._reihenfolge = reihenfolge
+        self._leistung = leistung
+        if wind not in ["", "----"]:
+            wind = wind.rstrip(" m*")
+            wind = wind.replace(",", ".")
+            wind = float(wind)
+        else:
+            wind = None
+        self._wind = wind
+        self._punkte = int(punkte)
+        self._last_disziplin = last_disziplin
+        assert self._leistung_divisor is not None
 
-    def _add_start(self, start):
-        disziplin = start.wettkampf.disziplin.kurzname
-        reihenfolge = start.wettkampf.mehrkampfreihenfolge
-        windmessung = start.wettkampf.windmessung
-        last_disziplin = start.wettkampf.mehrkampfende
-        serienstart = start.serienstart.first()
-        if serienstart is None:
-            return
+    @property
+    def name(self):
+        return self._name
 
-        resultate = serienstart.resultat.order_by("-punkte").all()
-        for resultat in resultate:
-            leistung = resultat.leistung
-            wind = ""
-            if windmessung:
-                wind = serienstart.serie.wind
-                if wind == "":
-                    wind = resultat.info
-            punkte = int(resultat.punkte)
-            if not resultat.info.lower().endswith("x"):
-                break
+    @property
+    def reihenfolge(self):
+        return self._reihenfolge
 
-        item = self._get_item(start.anmeldung.athlet)
-        item.add_disziplin(disziplin, reihenfolge, last_disziplin, leistung,
-                           wind, punkte)
+    @property
+    def leistung(self):
+        return self._leistung
 
-    def _get_item(self, athlet):
-        """get ranglisten item
+    @property
+    def wind(self):
+        return self._wind
 
-        Get existing or new ranglisten item.
-        """
-        try:
-            item = self._starts[athlet.id]
-        except KeyError:
-            item = RanglistenItem(athlet.name, athlet.vorname, athlet.jahrgang,
-                                  athlet.verein, athlet.land, "")
-            self._starts[athlet.id] = item
-        return item
+    @property
+    def punkte(self):
+        return self._punkte
 
-    def get(self):
-        items = sorted(self._starts.values(), reverse=True)
-        rangliste = []
-        for item in items:
-            if item._num_valid_disziplinen() < len(item._disziplinen):
-                rang = ""
-            elif len(rangliste) > 0 and item == rangliste[-1][1]:
-                rang = rangliste[-1][0]
-            else:
-                rang = "%d." % (len(rangliste) + 1)
-            rangliste.append((rang, item))
-        return rangliste
+    @property
+    def last_disziplin(self):
+        return self._last_disziplin
+
+    def __str__(self):
+        text = "%s" % self.name
+        if self.leistung in self._LEISTUNG_MAPPING:
+            text += " (%s, %s)" % (self.leistung, self._LEISTUNG_MAPPING[self.leistung])
+            assert self.punkte == 0
+            return text
+        text += " (%s" % self._get_leistung_as_string()
+        if self.wind is not None:
+            text += " / %.1f"  % self.wind
+        text += ", %s)"  % self.punkte
+        return text
+
+    def __cmp__(self, other):
+        value = cmp(self.last_disziplin, other.last_disziplin)
+        if value != 0:
+            return value
+        value = cmp(self.reihenfolge, other.reihenfolge)
+        if value != 0:
+            return value
+        value = cmp(self.name, other.name)
+        if value != 0:
+            return value
+
+
+class MilliSecondsLeistungDisziplin(DisziplinBase):
+    _leistung_divisor = 1000
+
+    def _get_leistung_as_string(self):
+        hours, rem = divmod(float(self.leistung) / self._leistung_divisor, 3600)
+        minutes, seconds = divmod(rem, 60)
+        timestring = "%d:%d:%.2f" % (hours, minutes, seconds)
+        timestring = timestring.lstrip("0:")
+        #print "hours=%d, minutes=%d, seconds=%.3f => %s" % (
+        #    hours, minutes, seconds, timestring)
+        return timestring
+
+class CentimeterLeistungDisziplin(DisziplinBase):
+    _leistung_divisor = 100
+
+    def _get_leistung_as_string(self):
+        return "%.2f" % (float(self.leistung) / self._leistung_divisor)
+
+
+def create_disziplin(name, reihenfolge, leistung, wind, punkte, last_disziplin):
+    if name[0].isdigit():
+        disziplin_class = MilliSecondsLeistungDisziplin
+    else:
+        disziplin_class = CentimeterLeistungDisziplin
+    return disziplin_class(name, reihenfolge, leistung, wind, punkte, last_disziplin)
