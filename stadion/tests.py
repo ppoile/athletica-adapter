8ec2bd08f8f06123e372823336c29cf9ee90b180
ppoile <andi@daemeier-clan.ch>
2015-01-16 00:13:30 +0100

    stadion: rework tests

--- stadion/tests.py
+++ stadion/tests.py
@@ -10,6 +10,7 @@ class AnlagenTests(TestCase):
         self.assertEqual(anlage.bezeichnung, "")
         self.assertEqual(anlage.homologiert, "y")
         self.assertEqual(anlage.stadion_id, None)
+        self.assertEqual(anlage.stadion, None)
 
     def test_bezeichnung(self):
         bezeichnung = "UMM"
@@ -22,16 +23,35 @@ class StadionTests(TestCase):
         stadion = Stadion()
         self.assertEqual(stadion.id, None)
         self.assertEqual(stadion.name, "")
-        self.assertEqual(stadion.bahnen, None)
-        self.assertEqual(stadion.bahnengerade, None)
+        self.assertEqual(stadion.bahnen, 6)
+        self.assertEqual(stadion.bahnengerade, 6)
         self.assertEqual(stadion.halle, "n")
         self.assertEqual(stadion.ueber1000m, "n")
+        self.assertEqual(stadion.anlagen.count(), 0)
 
-    def test_object_creation(self):
-        stadion = Stadion.objects.create(name="Buchholz")
+    def test_object_save_and_get(self):
+        stadion = Stadion(name="Buchholz")
+        stadion.save()
+        stadion_id = stadion.id
+        self.assertNotEqual(stadion_id, None)
+        Stadion.objects.get(id=stadion_id)
+
+    def test_object_create_and_get(self):
+        stadion = Stadion.objects.create()
+        stadion_id = stadion.id
+        self.assertNotEqual(stadion_id, None)
+        Stadion.objects.get(id=stadion_id)
 
     def test_stadion_anlage_relation(self):
-        anlage = Anlage()
-        stadion = Stadion()
-        anlage.stadion = stadion
-        self.assertEqual(anlage.stadion_id, stadion.id)
+        stadion = Stadion.objects.create(name="Buchholz")
+        self.assertEqual(stadion.anlagen.count(), 0)
+
+        bezeichnung = "Hochsprung"
+        stadion.anlagen.create(bezeichnung=bezeichnung)
+        self.assertEqual(stadion.anlagen.count(), 1)
+        self.assertEqual(stadion.anlagen.first().bezeichnung, bezeichnung)
+
+        anlage = Anlage(bezeichnung="Weitsprung")
+        stadion.anlagen.add(anlage)
+        self.assertEqual(stadion.anlagen.count(), 2)
+        self.assertIn(anlage, list(stadion.anlagen.all()))
