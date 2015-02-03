684f3bd743f00f27fb7e09f7425c682ef6faa5cf
Andreas Meier <andi@daemeier-clan.ch>
2015-02-03 02:56:05 +0100

    athletica: fix staticfiles

--- athletica/settings.py
+++ athletica/settings.py
@@ -89,8 +89,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "athletica/static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "collected-staticfiles")
 
 WEBODT_CONVERTER = 'webodt.converters.abiword.AbiwordODFConverter'
 WEBODT_TEMPLATE_PATH = ''
