b0d96cff4ac1eee37d975960c7506ccbcad9f0c9
Andreas Meier <andi@daemeier-clan.ch>
2015-03-27 00:09:12 +0100

    main: rework fix_db-script

--- main/bin/fix_db.py
+++ main/bin/fix_db.py
@@ -1,25 +1,26 @@
 from main import models
 
-def fix_db_main():
+def fix_db_main(dry_run=False):
     bad_serien = models.Serie.objects.filter(anlage_id=0)
     if bad_serien.count() > 0:
-        bad_serien.update(anlage_id=None)
-        print "model Serie fixed"
+        print "there are %d series with anlage_id=0..." % bad_serien.count(),
+        if not dry_run:
+            bad_serien.update(anlage_id=None)
+            print "fixed"
+        else:
+            print
     else:
-        print "model Serie is fine"
+        print "series are fine"
 
     bad_starts = models.Start.objects.filter(staffel_id=0)
     if bad_starts.count() > 0:
-        bad_starts.update(staffel_id=None)
-        print "model Start fixed"
+        print "there are %d starts with staffel_id=0..." % bad_starts.count(),
+        if not dry_run:
+            bad_starts.update(staffel_id=None)
+            print "fixed"
+        else:
+            print
     else:
-        print "model Start is fine"
-
-    bad_rundensets = models.Rundenset.objects.filter(hauptrunde=None)
-    if bad_rundensets.count() > 0:
-        bad_rundensets.update(hauptrunde=0)
-        print "model Rundenset fixed"
-    else:
-        print "model Rundenset is fine"
+        print "starts are fine"
 
     print "done"
